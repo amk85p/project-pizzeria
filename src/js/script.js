ae5ee745acdb72e363ceb9b9a03ae3ba75417e4c
Anna Kalinowska <mirska.anna@gmail.com>
2022-08-29 10:56:32 +0200

    init accordion

--- src/js/script.js
+++ src/js/script.js
@@ -90,9 +90,7 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector(
-          select.all.menuProductsActive
-        );
+        const activeProduct = document.querySelector('.product.active');
         console.log(activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct !== null && activeProduct !== thisProduct.element) {
